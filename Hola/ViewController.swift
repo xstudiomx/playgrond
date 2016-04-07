0a597ef1332cc31bcfe73b42ca7dd62bc0e5ae57
xstudiomx <ventas@xstudio.mx>
2016-04-07 15:51:35 -0500

    Initial Commit

---
+++ Hola/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola
+//
+//  Created by edgar lopez on 07/04/16.
+//  Copyright © 2016 Xstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
